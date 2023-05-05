fd80b589750b3457ba9cb0746f18ae39a2874692
SunDoge <384813529@qq.com>
2023-05-05 22:45:46 +0800

    update

--- src/tensor.rs
+++ src/tensor.rs
@@ -116,139 +116,27 @@ impl<'a> TensorBuilder<'a> {
     }
 }
 
-pub struct ManagerContext<T> {
-    pub ptr: Option<NonNull<*mut c_void>>,
-    _type: PhantomData<T>,
-    _pin: PhantomPinned,
+pub struct ManagedTensor<C> {
+    pub inner: DLManagedTensor,
+    _marker: PhantomData<C>,
 }
 
-impl<T> ManagerContext<T> {
-    pub fn new(ptr: Option<NonNull<*mut c_void>>) -> Self {
-        Self {
-            ptr,
-            _type: PhantomData,
-            _pin: PhantomPinned,
-        }
-    }
+unsafe extern "C" fn deleter_fn<T>(dl_managed_tensor: *mut DLManagedTensor) {
+    let ctx = (*dl_managed_tensor).manager_ctx as *mut T;
+
+    drop(unsafe { Box::from_raw(ctx) });
+    // println!("drop ctx: {:?}", ctx);
 }
 
-#[pin_project(PinnedDrop)]
-pub struct ManagedTensorProxy<T> {
-    pub dl_tensor: DLTensor,
-    #[pin]
-    pub manager_ctx: ManagerContext<T>,
-    pub deleter: Option<fn(&mut ManagedTensor<T>)>,
-}
-
-impl<T> From<DLManagedTensor> for ManagedTensorProxy<T> {
-    fn from(mut value: DLManagedTensor) -> Self {
-        let ptr = if value.manager_ctx.is_null() {
-            None
-        } else {
-            Some(unsafe { NonNull::new_unchecked(&mut value.manager_ctx as *mut _) })
-        };
-
-        let manager_ctx = ManagerContext::new(ptr);
-        let deleter = value.deleter.take().map(|del_fn| unsafe {
-            transmute::<unsafe extern "C" fn(*mut DLManagedTensor), fn(&mut ManagedTensor<T>)>(
-                del_fn,
-            )
-        });
-
-        Self {
-            dl_tensor: value.dl_tensor,
-            manager_ctx,
-            deleter,
-        }
-    }
-}
-
-impl<T> From<ManagedTensorProxy<T>> for DLManagedTensor {
-    fn from(value: ManagedTensorProxy<T>) -> Self {
-        let dl_tensor = value.dl_tensor;
-        let manager_ctx = match value.manager_ctx.ptr {
-            None => ptr::null_mut(),
-            Some(non_null) => unsafe { *non_null.as_ptr() },
-        };
-
-        let deleter = value.deleter.map(|del_fn| unsafe {
-            transmute::<fn(&mut ManagedTensor<T>), unsafe extern "C" fn(*mut DLManagedTensor)>(
-                del_fn,
-            )
-        });
-        DLManagedTensor {
+impl<C> ManagedTensor<C> {
+    pub fn new(ctx: C, dl_tensor: DLTensor) -> Self {
+        let dl_managed_tensor = DLManagedTensor {
             dl_tensor,
-            manager_ctx,
-            deleter,
-        }
-    }
-}
-
-impl<T> From<Pin<&mut ManagedTensorProxy<T>>> for DLManagedTensor {
-    fn from(value: Pin<&mut ManagedTensorProxy<T>>) -> Self {
-        let dl_tensor = value.dl_tensor;
-        let manager_ctx = match value.manager_ctx.ptr {
-            None => ptr::null_mut(),
-            Some(non_null) => unsafe { *non_null.as_ptr() },
+            manager_ctx: Box::into_raw(Box::new(ctx)) as *mut _,
+            deleter: Some(deleter_fn::<C>),
         };
-
-        let deleter = value.deleter.map(|del_fn| unsafe {
-            transmute::<fn(&mut ManagedTensor<T>), unsafe extern "C" fn(*mut DLManagedTensor)>(
-                del_fn,
-            )
-        });
-        DLManagedTensor {
-            dl_tensor,
-            manager_ctx,
-            deleter,
-        }
-    }
-}
-
-#[pinned_drop]
-impl<T> PinnedDrop for ManagedTensorProxy<T> {
-    fn drop(mut self: Pin<&mut Self>) {
-        let mut dl_managed_tensor: DLManagedTensor = self.as_mut().into();
-        if let Some(del_fn) = self.deleter {
-            let c_del_fn =
-                unsafe { transmute::<fn(&mut ManagedTensor<T>), fn(*mut DLManagedTensor)>(del_fn) };
-            c_del_fn(&mut dl_managed_tensor as *mut _);
-        }
-    }
-}
-
-pub struct ManagedTensor<'a, T: 'a> {
-    pub proxy: ManagedTensorProxy<T>,
-    _marker: PhantomData<fn(&'a ()) -> &'a ()>, // invariant wrt 'tensor
-}
-
-impl<'a, T> From<DLManagedTensor> for ManagedTensor<'a, T> {
-    fn from(value: DLManagedTensor) -> Self {
-        let proxy = value.into();
         Self {
-            proxy,
-            _marker: PhantomData,
-        }
-    }
-}
-
-impl<'a, T> From<ManagedTensor<'a, T>> for DLManagedTensor {
-    fn from(value: ManagedTensor<'a, T>) -> Self {
-        value.proxy.into()
-    }
-}
-
-impl<'a, T: 'a> ManagedTensor<'a, T> {
-    pub fn new(tensor_ref: Tensor<'a>, manager_ctx: Option<NonNull<*mut c_void>>) -> Self {
-        let manager_ctx = ManagerContext::new(manager_ctx);
-        let proxy = ManagedTensorProxy {
-            dl_tensor: tensor_ref.into_inner(),
-            manager_ctx,
-            deleter: None,
-        };
-
-        Self {
-            proxy,
+            inner: dl_managed_tensor,
             _marker: PhantomData,
         }
     }
@@ -267,5 +155,27 @@ mod tests {
         let v2: &[f32] =
             unsafe { slice::from_raw_parts(tensor.data() as *const _, tensor.shape()[0] as usize) };
         dbg!(v2);
+    }
+
+    #[test]
+    fn from_vec_f32_managed() {
+        struct V(Vec<f32>);
+
+        impl Drop for V {
+            fn drop(&mut self) {
+                println!("drop v");
+                drop(self);
+            }
+        }
+
+        let v: Vec<f32> = (0..10).map(|x| x as f32).collect();
+        let mut v = V(v);
+        let shape = vec![v.0.len() as i64];
+        let tensor = TensorBuilder::new(v.0.as_mut_ptr() as *mut _, &shape).build();
+        dbg!(&tensor);
+        let managed_tensor = ManagedTensor::new(v, tensor.into());
+        managed_tensor.inner.deleter.map(|del_fn| unsafe {
+            del_fn(&managed_tensor.inner as *const _ as *mut _);
+        });
     }
 }
