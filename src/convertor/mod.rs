8677ae8e321c3f8ecdeb871a01398d7cc754f6e7
SunDoge <triplez0@outlook.com>
2025-05-31 10:37:36 +0800

    update

--- src/convertor/mod.rs
+++ src/convertor/mod.rs
@@ -2,3 +2,7 @@ pub mod std_container;
 
 #[cfg(feature = "image")]
 pub mod image;
+
+
+#[cfg(feature = "pyo3")]
+pub mod python;
--- src/convertor/python.rs
+++ src/convertor/python.rs
@@ -1,1 +1,13 @@
+use std::ffi::CString;
 
+// const DLTENSOR: &[u8] = b"dltensor\0";
+// const USED_DLTENSOR: &[u8] = b"used_dltensor\0";
+// const DLTENSOR_VERSIONED: &[u8] = b"dltensor_versioned\0";
+// const USED_DLTENSOR_VERSIONED: &[u8] = b"used_dltensor_versioned\0";
+
+
+
+
+unsafe extern "C" fn dlpack_capsule_deleter(capsule: *mut pyo3::ffi::PyObject) {
+    unsafe { if pyo3::ffi::PyCapsule_IsValid(capsule, F.as_ptr() as *const i8) == 1 {} }
+}
