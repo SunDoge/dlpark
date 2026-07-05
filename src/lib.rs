45c752252bc6a3ecf4ef641dda420e61a9e692f9
SunDoge <triplez0@outlook.com>
2026-07-05 22:48:11 +0800

    refact: rename builder module to boxed to match Rust naming conventions

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 pub mod ffi;
 
-pub mod builder;
+pub mod boxed;
 pub mod context;
 
 mod data_type;
@@ -9,6 +9,6 @@ mod managed_tenesor_versioned;
 mod managed_tensor;
 mod tensor;
 
-pub use builder::DlpackBox;
+pub use boxed::DlpackBox;
 pub use managed_tenesor_versioned::DlpackFlags;
 pub use managed_tensor::{Dlpack, ManagedTensor};
